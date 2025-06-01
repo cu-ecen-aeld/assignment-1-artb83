b198958a2e4a4cc03ed1872771a9680e79b30124
artb83 <21041434+artb83@users.noreply.github.com>
2025-06-01 22:59:03 +0300

    Adding notes to the script and removing print messages.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#$1 Full path to the file.
+#$2 String to write into the file that pointed by the $1 path.
 
 FILE=0
 DIRECTORY=$1
@@ -9,25 +11,21 @@ if [ $# != 2 ]; then
     echo "Invalid parameters."
     exit 1
 fi
-#If provided file doesn't exist, create a file
+#If provided file or directory doesn't exist, create a directory and a file
 if [ ! -f $1 ]; then
     FILE=${DIRECTORY##*/}
-    echo "file "$FILE
     DIRECTORY=${DIRECTORY%/*}
     DIRECTORY=$DIRECTORY"/"
-    echo "dir "$DIRECTORY
     if [ ! -d $DIRECTORY ]; then
         mkdir $DIRECTORY
     fi
     FULL_PATH=$DIRECTORY$FILE
-    echo "full path "$FULL_PATH
     touch $FULL_PATH
 fi
 
-#If a file was created successfully, write a 2nd parameter content to it
+#If a file was created successfully, write desired content ($2) to it, or report an error.
 if [ -f $1 ]; then
     echo $2>$1
-    echo "OK "'$1'" "$1
 else
     echo "Could not create a file "$1
     exit 1
