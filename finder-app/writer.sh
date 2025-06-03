88720ac44e99e138a1a593aeb02e73cfccfa3888
artb83 <21041434+artb83@users.noreply.github.com>
2025-06-03 15:44:55 +0300

    Fixing writer.sh. Break down the path ($1) into separate items and build each directory on the that path, eventually creating a file. Remove echos.

--- finder-app/writer.sh
+++ finder-app/writer.sh
@@ -4,27 +4,40 @@
 
 FILE=0
 DIRECTORY=$1
-FULL_PATH=0
+FULL_PATH=$1
 
 #Check valid amount of parameters
 if [ $# != 2 ]; then
     echo "Invalid parameters."
     exit 1
 fi
+
 #If provided file or directory doesn't exist, create a directory and a file
 if [ ! -f $1 ]; then
     FILE=${DIRECTORY##*/}
     DIRECTORY=${DIRECTORY%/*}
     DIRECTORY=$DIRECTORY"/"
-    if [ ! -d $DIRECTORY ]; then
-        mkdir $DIRECTORY
-    fi
-    FULL_PATH=$DIRECTORY$FILE
+    #Make temp path with ':' as separator instead of '/'
+    TEMP_PATH="${DIRECTORY//'/'/":"}"
+    ROOT=""
+    IFS=:
+    #Break temporary path into separate items using ':' as separator.
+    #Build a directory path from separate items and check if it exists.
+    #If the directory doesn't exist create (mkdir) it.
+    for item in $TEMP_PATH; do
+        ROOT=$ROOT${item}"/"
+        if [ ! -d $ROOT ]; then
+            mkdir $ROOT
+        fi
+    done
+
+    #Create a new file at full path.
+    FULL_PATH=$ROOT$FILE
     touch $FULL_PATH
 fi
 
-#If a file was created successfully, write desired content ($2) to it, or report an error.
-if [ -f $1 ]; then
+#If a file was created successfully, write desired content ($2) to it, or report an error
+if [ -f $FULL_PATH ] && [ $1=$FULL_PATH ]; then
     echo $2>$1
 else
     echo "Could not create a file "$1
